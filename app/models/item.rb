da3bd99e1e6e69136032d26ce8ba65008f84affb
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-07-22 12:02:33 +0900

    商品出品画面への遷移

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
-#  validates :name, presence: true
-#  validates :description, presence: true
-#  validates :charge_id, presence: true
-#  validates :price, presence: true
-#  validates :category_id, presence: true
-#  validates :status_id, presence: true
-#  validates :prefecture_id, presence: true
-#  validates :shipping_day_id, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :charge_id, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
 
-#  belongs_to :user
-#  has_one :purchase
+  belongs_to :user
+  has_one :purchase
+  has_one_attached :image
 end
