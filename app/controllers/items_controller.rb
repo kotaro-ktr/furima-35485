f8c2a09dad083f8e1f315f110304d394b583abb9
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-07-24 10:30:48 +0900

    商品の出品とDBへの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
-    @items = Item.all
+    #@items = Item.all
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :image, :category_id, :status_id, :charge_id, :prefecture_id, :shipment_id).merge(user_id: current_user.id)
   end
 
 end
