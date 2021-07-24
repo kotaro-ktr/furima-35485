77c5a482574ddccd271860fe0fe114d1820d14ed
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-07-24 10:28:33 +0900

    プルダウンデータの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,28 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :charge_id, presence: true
-  validates :price, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    validates :price, numericality: {only_integer: true, grater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is invalid. Input half-width characters'}
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
+    validates :charge_id
+    validates :category_id
+    validates :status_id
+    validates :prefecture_id
+    validates :shipment_id
+  end
 
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge
+  belongs_to :prefecture
+  belongs_to :shipment
+
 end
