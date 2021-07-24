933f64a94570c6202d09f344fcee073b1d400cde
kotaro-ktr <83695177+kotaro-ktr@users.noreply.github.com>
2021-07-24 10:29:17 +0900

    カラム名の整理

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_07_22_020359) do
     t.integer "category_id", null: false
     t.integer "status_id", null: false
     t.integer "prefecture_id", null: false
-    t.integer "shipping_day_id", null: false
+    t.integer "shipment_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
